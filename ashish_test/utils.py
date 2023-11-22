0631dd2003ee3fc4f524f241865d7ea2d072bf31
ASHISH DHIMAN <ashish1610dhiman@gmail.com>
2023-11-21 21:11:28 -0500

    finetune testing

--- ashish_test/utils.py
+++ ashish_test/utils.py
@@ -62,4 +62,35 @@ def format_steps(steps):
 
     return ' ####### '.join(formatted_steps)
 
+from torch.utils.data import Dataset
+import torch
+
+class FinQA_Dataset(Dataset):
+    def __init__(self, tokenizer, df, max_q_len, max_a_len):
+        self.tokenizer = tokenizer
+        self.q_len = max_q_len
+        self.t_len = max_a_len
+        self.data = df
+        self.questions = self.data["question"]
+        self.context = self.data["context"]
+        self.answer = self.data['answer']        
+    def __len__(self):
+        return len(self.questions)
+    def __getitem__(self, idx):
+        question = self.questions[idx]
+        context = self.context[idx]
+        answer = self.answer[idx]
+        question_tokenized = self.tokenizer(question, context, max_length=self.q_len, padding="max_length",
+                                                    truncation=True, pad_to_max_length=True, add_special_tokens=True)
+        answer_tokenized = self.tokenizer(answer, max_length=self.t_len, padding="max_length", 
+                                          truncation=True, pad_to_max_length=True, add_special_tokens=True)
+        labels = torch.tensor(answer_tokenized["input_ids"], dtype=torch.long)
+        # labels[labels == 0] = -100
+        
+        return {
+            "input_ids": torch.tensor(question_tokenized["input_ids"], dtype=torch.long),
+            "attention_mask": torch.tensor(question_tokenized["attention_mask"], dtype=torch.long),
+            "labels": labels,
+            "decoder_attention_mask": torch.tensor(answer_tokenized["attention_mask"], dtype=torch.long)
+        }
 
